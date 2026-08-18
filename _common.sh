c53b04023bdb307176727a953a27d1adbd1afca2
Skruvfejs <johan.m.harwell@gmailcom>
2026-08-18 14:56:04 +0200

    Modified _common.sh

--- _common.sh
+++ _common.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+
 USER="screwface"
 
-# Fixing sudo
-su
-/usr/sbin/usermod -aG sudo $USER
 
-id screwface
-# Returning groups
-# uid=1000(screwface) gid=1000(screwface) grupper=1000(screwface),24(cdrom),25(floppy),27(sudo),29(audio),30(dip),44(video),46(plugdev),100(users),101(netdev)
-#                                                                                      ^^^^^^^^ 
+# ================================================================================================
+# SUDO
+# ================================================================================================
+# Starta om scriptet med sudo om det inte redan körs som root
+if [[ $EUID -ne 0 ]]; then
+    exec sudo "$0" "$@"
+fi
 
+# Lägg till användaren i sudo-gruppen
+usermod -aG sudo "$USER"
+echo "Grupper för $USER:"
+id "$USER"
+
+
+
+
+# ===============================================================================================
 # Fixing ll command
+# ===============================================================================================
 # Usage: ll
 echo "alias ll='ls -lah --color=auto'" >> ~/.bashrc
 source ~/.bashrc
