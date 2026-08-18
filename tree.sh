d2f14301a4561d307e3500cbf662be4e1e4d56c0
screwface <skruvfejs@proton.me>
2026-08-18 09:35:23 +0200

    Modified tree.sh

--- tree.sh
+++ tree.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt tree
+sudo apt install tree
 
 tree
 
-# screwface@mail:~/Projects/screwface-firewall$ tree
+# screwface@linuxmint:~/Projects/linux_stuff$ tree
 # .
-# ├── allowlist.conf.example
-# ├── denylist.conf.example
-# ├── firewall.conf.example
-# ├── install.sh
-# ├── LICENSE
-# ├── lists
-# │   ├── mailclients.allow.example
-# │   ├── smtp.deny.example
-# │   ├── ssh.allow.example
-# │   ├── vpn.allow.example
-# │   └── web.allow.example
-# ├── logs
-# │   └── screwface-firewall.log -> /var/log/screwface-firewall.log
-# ├── patch_mailclients_allow.py
-# ├── ports.conf.example
-# ├── README.md
-# ├── scripts
-# │   └── screwface-firewall.sh
-# ├── special.conf.example
-# └── uninstall.sh
+# ├── file1.examplea
+# ├── file2..example
+# ├── folder1
+# │   ├── file03..example
+# │   └── file04.example
+# └── file05.example
 
